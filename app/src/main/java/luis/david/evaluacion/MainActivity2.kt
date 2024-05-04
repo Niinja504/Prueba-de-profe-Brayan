dfaedad689ee59ca0a37917b8095746de3bacd0f
D <Anonimo>
2024-05-03 23:32:30 -0600

    Version 2

--- app/src/main/java/luis/david/evaluacion/MainActivity2.kt
+++ app/src/main/java/luis/david/evaluacion/MainActivity2.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val botonAbrirActividad = findViewById<Button>(R.id.btn_Ingresar)
+
+           botonAbrirActividad.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java) // Reemplazar MainActivity con el nombre real de tu actividad
+            startActivity(intent)
+        }
     }
+
 }
